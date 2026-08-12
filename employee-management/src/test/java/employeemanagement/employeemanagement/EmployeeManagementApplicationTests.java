512f46a9d42726113df3f17e5d008b4d9b232b9e
Aditya Kulkarni <addkulkarni@gmail.com>
2026-08-12 17:54:34 +0530

    Initial employee-management setup

---
+++ employee-management/src/test/java/employeemanagement/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package employeemanagement.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
